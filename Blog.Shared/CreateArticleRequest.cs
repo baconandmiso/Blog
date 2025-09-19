c2f20a84514554fb96942547912280128fe00612
baconandmiso <Kinosoft@outlook.jp>
2025-09-19 09:33:40 +0900

    add: 記事の作成用, 更新用のDTOクラスをここで表します。

---
+++ Blog.Shared/CreateArticleRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Shared;
+
+public class CreateArticleRequest
+{
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public IEnumerable<long> CategoryIds { get; set; }
+}
---
+++ Blog.Shared/UpdateArticleRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Shared;
+
+public class UpdateArticleRequest
+{
+    public string? Title { get; set; }
+
+    public string? Content { get; set; }
+
+    public IEnumerable<long>? CategoryIds { get; set; }
+}
