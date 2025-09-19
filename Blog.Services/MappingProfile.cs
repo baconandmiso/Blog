225150e92779b8f410c6b9d44e1eb0242cb7f8e5
baconandmiso <Kinosoft@outlook.jp>
2025-09-19 09:40:12 +0900

    add: AutoMapperによるマッピングルールを定義します。

---
+++ Blog.Services/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Blog.Entity;
+using Blog.Shared;
+
+namespace Blog.Services;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<UpdateArticleRequest, Article>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+    }
+}
