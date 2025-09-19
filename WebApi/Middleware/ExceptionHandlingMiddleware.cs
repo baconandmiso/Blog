5c0076b8a159337510f7fdeab2b05a2608241e1b
baconandmiso <Kinosoft@outlook.jp>
2025-09-19 09:35:14 +0900

    add: グローバル例外処理ミドルウェア

---
+++ WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Services;
+
+namespace WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (EntityNotFoundException)
+        {
+            context.Response.StatusCode = StatusCodes.Status404NotFound;
+            await context.Response.WriteAsJsonAsync(new { message = "404 Not Found" });
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await context.Response.WriteAsJsonAsync(new { error = "An unexpected error occurred." });
+        }
+    }
+}
