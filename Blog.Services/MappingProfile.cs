d45052de9c262125bcb4bc47ac86ba4d6dd37bcb
baconandmiso <Kinosoft@outlook.jp>
2025-09-21 07:48:55 +0900

    add: マッピングルールに 記事作成, カテゴリ作成・更新を追加。

--- Blog.Services/MappingProfile.cs
+++ Blog.Services/MappingProfile.cs
@@ -8,7 +8,16 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        CreateMap<CreateArticleRequest, Article>()
+            .ForMember(dest => dest.ArticleCategories, opt => opt.Ignore());
+
+        CreateMap<CreateCategoryRequest, Category>()
+            .ForMember(dest => dest.ArticleCategories, opt => opt.Ignore());
+
         CreateMap<UpdateArticleRequest, Article>()
+            .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+        CreateMap<UpdateCategoryRequest, Category>()
             .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
     }
 }
