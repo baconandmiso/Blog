c22b3f31472b1ff770296fcf4b5235b8afbf638f
baconandmiso <Kinosoft@outlook.jp>
2025-09-21 00:14:56 +0900

    update: ドキュメントを更新した。 プロパティ Title と Contentをrequiredにした。 プロパティ ArticleCategories のセッターをプライベートにし, 意図しない動作を起こさないようにした。

--- Blog.Entity/Article.cs
+++ Blog.Entity/Article.cs
@@ -1,52 +1,54 @@
 ﻿namespace Blog.Entity;
 
 /// <summary>
-///　記事を表すエンティティ
+///　記事を表すエンティティ。
 /// </summary>
 public class Article
 {
     /// <summary>
-    /// 記事の一意な識別子
+    /// 記事の一意なIDを取得または設定します。
     /// </summary>
     public required long Id { get; set; }
 
     /// <summary>
-    /// 記事のタイトル
+    /// 記事のタイトルを取得または設定します。
     /// </summary>
-    public string Title { get; set; }
+    public required string Title { get; set; }
 
     /// <summary>
-    /// 記事の内容
+    /// 記事の本文を取得または設定します。
     /// </summary>
-    public string Content { get; set; }
+    public required string Content { get; set; }
 
     /// <summary>
-    /// サムネイル画像のURL
+    /// 記事のサムネイル画像のURLを取得または設定します。
     /// </summary>
     public string? ThumbnailUrl { get; set; }
 
     /// <summary>
-    /// 公開状態を示すフラグ
+    /// 記事が公開されているかどうかを示す値を取得または設定します。
     /// </summary>
     public bool IsPublished { get; set; }
 
     /// <summary>
-    /// 記事の作成日時
+    /// 記事の作成日時を取得または設定します。
     /// </summary>
     public DateTimeOffset CreatedAt { get; set; }
 
     /// <summary>
-    /// 記事の更新日時
+    /// 記事の最終更新日を取得または設定します。
+    /// 更新されていない場合はnullになります。
     /// </summary>
     public DateTimeOffset? UpdatedAt { get; set; }
 
     /// <summary>
-    /// 記事を公開した日時
+    /// 記事の公開日時を取得または設定します。
+    /// 公開されていない場合はnullになります。
     /// </summary>
     public DateTimeOffset? PublishedAt { get; set; }
 
     /// <summary>
-    /// カテゴリー
+    /// この記事に関連付けられているカテゴリのコレクションを取得します。
     /// </summary>
-    public ICollection<ArticleCategory> ArticleCategories { get; set; } = new List<ArticleCategory>();
+    public ICollection<ArticleCategory> ArticleCategories { get; private set; } = new List<ArticleCategory>();
 }
