dbab87e47d30e8ecb6a46a34163fb148ebdba235
baconandmiso <Kinosoft@outlook.jp>
2025-09-29 16:40:04 +0000

    update: マッピングルール変更

--- Blog.Services/MappingProfile.cs
+++ Blog.Services/MappingProfile.cs
@@ -20,10 +20,20 @@ public class MappingProfile : IRegister
         // Mapsterでは、デフォルトでnullのプロパティは無視されます。
         // 明示的に設定する場合は以下のようにします。
         config.NewConfig<UpdateArticleRequest, Article>()
-            .IgnoreNullValues(true);
+            .IgnoreNullValues(true)
+            .Ignore(dest => dest.ArticleCategories);
 
         // UpdateCategoryRequest から Category へのマッピング
         config.NewConfig<UpdateCategoryRequest, Category>()
             .IgnoreNullValues(true);
+
+        // Category から CategoryResponse へのマッピング
+        config.NewConfig<Category, CategoryResponse>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Name, src => src.Name);
+
+        // Article から ArticleResponse へのマッピング
+        config.NewConfig<Article, ArticleResponse>()
+            .Map(dest => dest.Categories, src => src.ArticleCategories.Select(ac　=> ac.Category));
     }
 }
