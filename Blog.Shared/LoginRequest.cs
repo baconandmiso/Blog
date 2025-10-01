3bb63d2c10d35bc82880ba899b9e4ed75b224209
baconandmiso <Kinosoft@outlook.jp>
2025-10-01 03:01:29 +0000

    update: LoginRequestクラスのUsernameプロパティの名前を修正

--- Blog.Shared/LoginRequest.cs
+++ Blog.Shared/LoginRequest.cs
@@ -5,7 +5,7 @@ namespace Blog.Shared;
 public class LoginRequest
 {
     [Required]
-    public string UserName { get; set; } = null!;
+    public string Username { get; set; } = null!;
 
     [Required]
     public string Password { get; set; } = null!;
