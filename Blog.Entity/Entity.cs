1f6acd2fbeec840c9d5fa695eb210e5292484314
baconandmiso <Kinosoft@outlook.jp>
2025-10-02 11:18:16 +0000

    feat: Entityクラスを新規作成し、一意の識別子プロパティを追加

---
+++ Blog.Entity/Entity.cs
@@ -0,0 +1,11 @@
+using System.Timers;
+
+namespace Blog.Entity;
+
+public abstract class Entity<TId> where TId : IEquatable<TId>
+{
+    /// <summary>
+    /// このオブジェクトの一意の識別子を取得します。
+    /// </summary>
+    public TId Id { get; protected set; }
+}
