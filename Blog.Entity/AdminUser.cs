6f8543e07b502886e961e3f1f9c79a04a5a7e2c8
baconandmiso <Kinosoft@outlook.jp>
2025-10-05 22:31:22 +0000

    feat: AdminUserクラスをSnowflakeEntityから継承し、プロパティを整理

--- Blog.Entity/AdminUser.cs
+++ Blog.Entity/AdminUser.cs
@@ -1,14 +1,10 @@
 namespace Blog.Entity;
 
-public class AdminUser
+public class AdminUser : SnowflakeEntity
 {
-    public long Id { get; set; }
-
     public required string Username { get; set; }
 
     public required string PasswordHash { get; set; }
 
     public string? TotpSecretKey { get; set; }
-
-    public DateTimeOffset CreatedAt { get; set; }
 }
