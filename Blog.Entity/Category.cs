d163c9ac9fada04557a2145e19472388e0d45b85
baconandmiso <Kinosoft@outlook.jp>
2025-10-05 22:31:28 +0000

    feat: CategoryクラスをSnowflakeEntityから継承し、記事カテゴリのプロパティを追加

--- Blog.Entity/Category.cs
+++ Blog.Entity/Category.cs
@@ -3,13 +3,8 @@
 /// <summary>
 /// 記事のカテゴリを表すエンティティ。
 /// </summary>
-public class Category
+public class Category : SnowflakeEntity
 {
-    /// <summary>
-    /// カテゴリの一意なIDを取得または設定します。
-    /// </summary>
-    public required long Id { get; set; }
-
     /// <summary>
     /// カテゴリの名前を取得または設定します。
     /// </summary>
@@ -18,5 +13,5 @@ public class Category
     /// <summary>
     /// カテゴリに関連付けられている記事のコレクションを取得します。
     /// </summary>
-    public ICollection<ArticleCategory> ArticleCategories { get; private　set; } = new List<ArticleCategory>();
+    public ICollection<ArticleCategory> ArticleCategories { get; private set; } = new List<ArticleCategory>();
 }
