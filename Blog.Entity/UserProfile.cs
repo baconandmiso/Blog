8ef3693555ba67ff1befca03a33759d478dc2282
baconandmiso <Kinosoft@outlook.jp>
2025-10-05 22:33:00 +0000

    feat: UserProfileクラスにSnowflakeEntityを継承し、必須のDisplayNameプロパティを追加

--- Blog.Entity/UserProfile.cs
+++ Blog.Entity/UserProfile.cs
@@ -1,13 +1,9 @@
 namespace Blog.Entity;
 
-public class UserProfile
+public class UserProfile : SnowflakeEntity
 {
-    public long UserId { get; set; } 
-    public string DisplayName { get; set; }
+    public required string DisplayName { get; set; }
     public string? ProfileText { get; set; }
     public string? IconUrl { get; set; }
     public string? HeaderUrl { get; set; }
-
-    // Userへのナビゲーションプロパティを追加
-    public User User { get; set; }
 }
