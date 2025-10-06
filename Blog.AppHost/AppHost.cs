1cbe01f8ba211efcb7945c5d2c84abc049bdebe6
baconandmiso <Kinosoft@outlook.jp>
2025-10-06 21:30:19 +0000

    chore: AppHost.csのコードを整理しました

--- Blog.AppHost/AppHost.cs
+++ Blog.AppHost/AppHost.cs
@@ -1,22 +1,36 @@
 ﻿using Aspire.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres", port: 54194)
-    .WithEnvironment("POSTGRES_DB", "blogdb")
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+}
+
+var pgPassword = builder.AddParameter("pgPassword");
+
+var postgres = builder.AddPostgres("postgres", password: pgPassword, port: 54194)
+    .WithEnvironment("POSTGRES_DB", "testdb")
     .WithDataVolume("VolumeMount.postgres.data")
     .WithPgAdmin(configureContainer: x => x.WithHostPort(54195))
-    .AddDatabase("PostgreSqlConnection", "blogdb");
+    .AddDatabase("blogdb", "testdb");
 
-var api = builder.AddProject<WebApi>("WebApi")
-        .WithReference(postgres);
+var migrations = builder.AddProject<Blog_MigrationService>("migrations")
+    .WithReference(postgres)
+    .WaitFor(postgres);
 
-var frontend = builder.AddNpmApp("Frontend", "../frontend", "dev")
+var webapi = builder.AddProject<Blog_WebApi>("webapi")
+        .WithReference(postgres)
+        .WithReference(migrations)
+        .WaitForCompletion(migrations);
+
+var frontend = builder.AddNpmApp("frontend", "../frontend", "dev")
     .WithHttpEndpoint(port: 8006, env: "PORT")
     .WithExternalHttpEndpoints()
-    .WithReference(api)
+    .WithReference(webapi)
     .WithReference(postgres);
 
 if (builder.Environment.IsDevelopment() && builder.Configuration["DOTNET_LAUNCH_PROFILE"] == "https")
