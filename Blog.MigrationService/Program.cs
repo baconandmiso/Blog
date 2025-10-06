5e2631254e5ea7d3269c37664c0a7e5c0e334d5f
baconandmiso <Kinosoft@outlook.jp>
2025-10-06 21:31:35 +0000

    feat: Blog.MigrationServiceプロジェクトを新規作成し、必要な設定ファイルとWorkerクラスを追加

---
+++ Blog.MigrationService/Program.cs
@@ -0,0 +1,15 @@
+using Blog.MigrationService;
+using Blog.Repository;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.AddServiceDefaults();
+builder.Services.AddHostedService<Worker>();
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing.AddSource(Worker.ActivitySourceName));
+
+builder.AddNpgsqlDbContext<ApplicationDbContext>("blogdb");
+
+var host = builder.Build();
+host.Run();
---
+++ Blog.MigrationService/Worker.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+using Blog.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.MigrationService;
+
+public class Worker : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+    public const string ActivitySourceName = "Migrations";
+
+    private static readonly ActivitySource s_activitySource = new ActivitySource(ActivitySourceName);
+
+    public Worker(IServiceProvider serviceProvider, IHostApplicationLifetime hostApplicationLifetime)
+    {
+        _serviceProvider = serviceProvider;
+        _hostApplicationLifetime = hostApplicationLifetime;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        using var activity = s_activitySource.StartActivity("Migrating Database", ActivityKind.Client);
+
+        try
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            await MigrationAsync(dbContext, stoppingToken);
+        }
+        catch (Exception ex)
+        {
+            activity?.AddException(ex);
+            throw;
+        }
+
+        _hostApplicationLifetime.StopApplication();
+    }
+
+    private static async Task MigrationAsync(ApplicationDbContext dbContext, CancellationToken cancellationToken)
+    {
+        var strategy = dbContext.Database.CreateExecutionStrategy();
+        await strategy.ExecuteAsync(async () =>
+        {
+            await dbContext.Database.MigrateAsync(cancellationToken);
+        });
+    }
+}
